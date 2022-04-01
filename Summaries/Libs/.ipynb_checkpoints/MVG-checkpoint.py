2b33bc9cf199dc27fe3ecc7bb9362bedff9a457c
MATTIA ROSSO s294711 <s294711@studenti.polito.it>
2022-04-01 14:56:38 +0200

    added lib file MVG

---
+++ Summaries/Libs/.ipynb_checkpoints/MVG-checkpoint.py
@@ -0,0 +1,39 @@
+import numpy as np
+def logpdf_GAU_ND_1sample(x,mu,C):
+    """This function computes the Gaussian probability density for a given sample x.
+        M: number of features
+        Parameters
+        ----------
+        x: sample | numpy array of shape (M, 1) 
+        mu: mean  | numpy array of shape (M, 1)
+        C: covariance matrix | numpy array of shape (M,M)
+        Returns
+        -------
+        N(x|mu,C) -> gaussian probability density associated to x given mu and C (it is a scalar)
+        """
+    M = x.shape[0]
+    xc = x - mu # x centered
+    invC = np.linalg.inv(C) # C^-1
+    _,log_abs_detC = np.linalg.slogdet(C) # log(abs(C))
+    return -M/2 * np.log(2*np.pi) - 1/2 * log_abs_detC - 1/2 * np.dot(np.dot(xc.T,invC),xc)
+
+def logpdf_GAU_ND(x,mu,C):
+    """This function computes the Gaussian probability density for a given set of samples x=(x1,x2,...xn).
+        M: number of features
+        N: number of samples
+        Parameters
+        ----------
+        x: matrix of samples | numpy array of shape (M, N) 
+        mu: mean  | numpy array of shape (M, 1)
+        C: covariance matrix | numpy array of shape (M,M)
+        Returns
+        -------
+        y=(N(x1|mu,C),N(x2|mu,C),...,N(xn|mu,C) -> gaussian probability densities associated to each sample xi given mu and C | numpy array of shape (N,)
+        """
+    M = x.shape[0]
+    N = x.shape[1]
+    y = np.zeros(N) # array of N scalar elements
+    for i in range(N):
+        density_xi = logpdf_GAU_ND_1sample(x[:,i:i+1],mu,C)
+        y[i] = density_xi
+    return y
